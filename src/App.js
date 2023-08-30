ee88e533f4eb953868587f0b7657f04270eeb8da
bintadam <zakiyaadan12@gmail.com>
2023-08-30 20:41:58 +0200

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import {useEffect, useState} from "react"
 
+
 function App() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState(null)
 
   useEffect(() => {
+    const url = "https://pokeapi.co/api/v2/pokemon"
+    axios
+      .get(url)
+      .then((response) => {
+      setData(response.data)
+    })
+      .catch((err) => {
+        console.error(err)
+    });
 
-  }) 
-  
+  }, []) 
+
   return (
     <div className="App">
 
