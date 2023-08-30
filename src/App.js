598a7371fa35fb3c33e6a94c2088fbcc3cf43d60
bintadam <zakiyaadan12@gmail.com>
2023-08-30 21:59:49 +0200

    added style

--- src/App.js
+++ src/App.js
@@ -24,19 +24,25 @@ function App() {
   }
 
   return (
-    <div className="">
-      {data.map((pokemon, index) => {
-        // Extract Pokémon ID from the URL
-        const id = pokemon.url.split("/")[6];
-        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    <div className="px-12 py-16">
+      <form className="flex place-content-center">
+        <input className="border-b-2 border-slate-800 focus:outline-none"/>
+        <button className="text-white bg-black py-2 px-4 rounded">Search</button>
+      </form>
+      <div className="grid grid-cols-5 gap-10">
+        {data.map((pokemon, index) => {
+          // Extract Pokémon ID from the URL
+          const id = pokemon.url.split("/")[6];
+          const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
-        return (
-          <div key={index}>
-            <img src={imageUrl} alt={pokemon.name} />
-            <p>{pokemon.name}</p>
-          </div>
-        );
-      })}
+          return (
+            <div key={index} className="p-16 border-2 border-slate-200 rounded-lg">
+              <img src={imageUrl} alt={pokemon.name} />
+              <p>{pokemon.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   )
 }
