7f7deff82ae5dc2433762c6419ae6ce2a4c455dd
bintadam <zakiyaadan12@gmail.com>
2023-09-01 14:52:25 +0200

    added search state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {useEffect, useState} from "react"
 import axios from "axios"
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
     const url = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
+    const search = `https://pokeapi.co/api/v2/pokemon?q=${searchTerm}`
     axios
-      .get(url)
+      .get(url, search)
       .then((response) => {
         // console.log(response.data)
         setData(response.data.results)
@@ -16,7 +18,11 @@ function App() {
       .catch((err) => {
         console.error(err)
     });
-
+    axios
+      .get(search)
+      .then((response) => {
+        setSearchTerm(searchTerm)
+      })
   }, []) 
 
   if(!data){
@@ -25,9 +31,9 @@ function App() {
 
   return (
     <div className="px-12 py-16">
-      <form className="flex place-content-center">
-        <input className="border-b-2 border-slate-800 focus:outline-none"/>
-        <button className="text-white bg-black py-2 px-4 rounded">Search</button>
+      <form className="flex place-content-center mb-12 ">
+        <input className="border border-slate-400 focus:outline-none mr-4 w-1/3 pl-4"/>
+        <button className="text-white bg-slate-600 py-2 px-6 rounded" on>Search</button>
       </form>
       <div className="grid grid-cols-5 gap-10">
         {data.map((pokemon, index) => {
@@ -36,7 +42,7 @@ function App() {
           const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
           return (
-            <div key={index} className="p-16 border-2 border-slate-200 rounded-lg">
+            <div key={index} className="p-16 border border-slate-300 rounded-lg">
               <img src={imageUrl} alt={pokemon.name} />
               <p>{pokemon.name}</p>
             </div>
