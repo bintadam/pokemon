c6beebd242d059d3d3e76484174a59cd5ee597f1
bintadam <zakiyaadan12@gmail.com>
2023-09-01 20:53:28 +0200

    added style

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios"
+import {BiSolidSortAlt} from "react-icons/bi"
 
 function PokemonList(){
     const [data, setData] = useState(null);
     const [searchTerm, setSearchTerm] = useState('')
-    const [ability, setAbility] = useState([])
+    // const [ability, setAbility] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
-          const url = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=100";
+          const url = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=150";
           try {
             const response = await axios.get(url);
             setData(response.data.results);
@@ -34,11 +35,11 @@ function PokemonList(){
     
     useEffect(() => {
         const fetchedData =async () => {
-            const abilityUrl = `https://pokeapi.co/api/v2/ability/{id}/`;
+            const abilityUrl = `https://pokeapi.co/api/v2/ability/`;
             try{
                 const response = await axios.get(abilityUrl);
                 console.log(response.data)
-                setAbility(response.data)
+                // setAbility(response.data)
             } catch (err) {
                 console.log(err)
             }
@@ -53,19 +54,22 @@ function PokemonList(){
     return (
         <div className="">
             <form className="flex place-content-center mb-12 " onSubmit={handleSearch}>
-                <input className="border border-slate-400 focus:outline-none mr-4 w-1/3 pl-4" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-                <button className="text-white bg-slate-600 py-2 px-6 rounded" type="submit">Search</button>
+                <input className="border border-slate-400 focus:outline-none mr-1 w-1/3 pl-4" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+                <button className="text-white bg-slate-600 py-2 px-6 rounded-sm mr-1" type="submit">Search</button>
+                <button className="bg-slate-600 py-2 px-4 rounded-sm">
+                    <BiSolidSortAlt className="text-white"/>
+                </button>
             </form>
-            <div className="grid grid-cols-6 gap-10">
+            <div className="grid grid-cols-6 gap-2">
             {data.map((pokemon, index) => {
             // Extract Pokémon ID from the URL
             const id = pokemon.url.split("/")[6];
             const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
             return (
-                <div key={index} className="p-16 border border-slate-300 rounded-lg">
-                <img src={imageUrl} alt={pokemon.name} />
-                <p>{pokemon.name}</p>
+                <div key={index} className="p-16 border border-slate-300 rounded bg-slate-400 text-white">
+                    <img src={imageUrl} alt={pokemon.name} />
+                    <p className="font-semibold">{pokemon.name.toUpperCase()}</p>
                 </div>
             );
             })}
